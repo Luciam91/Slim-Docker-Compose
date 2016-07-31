a70a00c4d15640c9d15e6a3fd1fcae0e7fcf32e7
Scott Pringle <pringlesc91@gmail.com>
2016-07-31 20:42:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Slim Framework 3 Skeleton with Docker-Compose
+
+Don't want to run Slim with the internal web server? Now you can run it with Docker! Just ensure you have docker-compose installed, clone the project and do a `docker-compose up` and you're ready to go!
+
+Further updates will follow including:
+
+- Adding a database container (MySQL / Postgres)
+- Adding a caching layer (Memcached/Redis)
+- Environment specific config files
---
+++ src/dependencies.php
@@ -0,0 +1,23 @@
+<?php
+// DIC configuration
+
+$container = $app->getContainer();
+
+// view renderer
+/**
+ * @param \Interop\Container\ContainerInterface $c
+ * @return \Slim\Views\PhpRenderer
+ */
+$container['renderer'] = function ($c) {
+    $settings = $c->get('settings')['renderer'];
+    return new Slim\Views\PhpRenderer($settings['template_path']);
+};
+
+// monolog
+$container['logger'] = function ($c) {
+    $settings = $c->get('settings')['logger'];
+    $logger = new Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+    return $logger;
+};
